882ee0f6c40b3c6b968a7e9420d5ae58b1034e1c
zuc1fer <zucifer0@gmail.com>
2025-12-13 20:00:46 +0100

    Add moveTo method to Position class for moving towards a destination

--- Position.java
+++ Position.java
@@ -16,4 +16,13 @@ public class Position{
     public double distanceTo(Position other) {
         return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
     }
+
+    public void moveTo(Position destination, double step) {
+        double distance = distanceTo(destination);
+        if (distance == 0) return;
+        double ratio = step / distance;
+        this.x = this.x + ratio * (destination.x - this.x);
+        this.y = this.y + ratio * (destination.y - this.y);
+    }
+
 }
