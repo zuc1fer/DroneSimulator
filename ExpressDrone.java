f33e95d986853b216b094f1b35c02e0704cc722d
zuc1fer <zucifer0@gmail.com>
2025-12-19 14:05:06 +0100

    feat: add ExpressDrone specific battery consumption calculation.

--- ExpressDrone.java
+++ ExpressDrone.java
@@ -3,4 +3,9 @@ public class ExpressDrone extends Drone{
 
         super(position , battery , "ExpressDrone" , 45 , 1);
     }
+
+    @Override
+    public double calculateConsumption(double distance) {
+        return distance * 3.0; // 3% per km
+    }
 }
