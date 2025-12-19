84026d5a4d195170584d23fc1033be9dd16bddb8
zuc1fer <zucifer0@gmail.com>
2025-12-19 16:14:10 +0100

    feat: Add constructor and update method to WeatherZone for managing its duration and state.

--- WeatherZone.java
+++ WeatherZone.java
@@ -5,4 +5,16 @@ public class WeatherZone extends NoFlyZone {
     private String weatherType;
     private int startTime;
 
+    public WeatherZone(Position center, double radius, String weatherType, int duration, int startTime) {
+        super(center, radius);
+        this.weatherType = weatherType;
+        this.duration = duration;
+        this.startTime = startTime;
+    }
+
+    public boolean update(int currentTime) {
+        int elapsed = currentTime - startTime;
+        duration--;
+        return duration > 0;
+    }
 }
