abe7f49fc6531536fd2364d05613554890531859
zuc1fer <zucifer0@gmail.com>
2025-12-19 16:15:05 +0100

    feat: add getters for weather properties and override toString method

--- WeatherZone.java
+++ WeatherZone.java
@@ -1,6 +1,5 @@
 public class WeatherZone extends NoFlyZone {
     
-    
     private int duration;
     private String weatherType;
     private int startTime;
@@ -16,5 +15,15 @@ public class WeatherZone extends NoFlyZone {
         int elapsed = currentTime - startTime;
         duration--;
         return duration > 0;
+    }
+
+    public String getWeatherType() { return weatherType; }
+    public int getDuration() { return duration; }
+    public int getStartTime() { return startTime; }
+
+    @Override
+    public String toString() {
+        return String.format("WeatherZone[%s, Radius: %.1fkm, Duration: %dmin left]", 
+            weatherType, getRadius(), duration);
     }
 }
