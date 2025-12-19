813300f5b3d4cd24f1f4f893c1f60972b0060163
zuc1fer <zucifer0@gmail.com>
2025-12-19 14:42:59 +0100

    feat: Add static fields to ControlCenter for tracking delivery statistics and drone energy consumption.

--- ControlCenter.java
+++ ControlCenter.java
@@ -7,5 +7,12 @@ public class ControlCenter {
     private Position base;
     private Map map;
     private List<Order> processedOrders;
- 
+    
+    private static int totalDeliveries = 0;
+    private static double totalDistance = 0;
+    
+    private static double standardDroneEnergy = 0;
+    private static double expressDroneEnergy = 0;
+    private static double heavyDroneEnergy = 0;
+
 }
