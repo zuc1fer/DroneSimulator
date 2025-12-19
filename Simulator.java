547f2a7890bf7b75b9477118bf04a511d640e6b8
zuc1fer <zucifer0@gmail.com>
2025-12-19 20:26:46 +0100

    refactor: remove unused `random` field

--- Simulator.java
+++ Simulator.java
@@ -5,13 +5,11 @@ import java.util.Map;
 import java.util.Random;
 
 public class Simulator {
-    
     private ControlCenter controlCenter;
     private List<Order> allOrders;
     private int currentMinute;
     private static final int SIMULATION_DURATION = 480;
     private Random random = new Random();
-
     private Map<Drone, Integer> deliveryCounts = new HashMap<>();
     private Map<Drone, Double> distanceTraveled = new HashMap<>();
     private Map<String, Integer> orderStatusCounts = new HashMap<>();
@@ -21,10 +19,12 @@ public class Simulator {
         this.controlCenter = controlCenter;
         this.allOrders = new ArrayList<>();
         this.currentMinute = 0;
+
         for (Drone drone : controlCenter.getDrones()) {
             deliveryCounts.put(drone, 0);
             distanceTraveled.put(drone, 0.0);
         }
+
         orderStatusCounts.put("PENDING", 0);
         orderStatusCounts.put("IN_PROGRESS", 0);
         orderStatusCounts.put("DELIVERED", 0);
