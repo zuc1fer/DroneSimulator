1c7d6bcef3fcb0097d2b157385e7f0c3ab3cca76
zuc1fer <zucifer0@gmail.com>
2025-12-19 14:26:29 +0100

    feat: Implement custom battery consumption and conditional speed for HeavyDrone, and correct drone type name.

--- HeavyDrone.java
+++ HeavyDrone.java
@@ -1,6 +1,19 @@
 public class HeavyDrone extends Drone{
     public HeavyDrone(Position position , double battery ){
 
-        super(position , battery , "HravyDrone" , 20 , 3);
+        super(position , battery , "HeavyDrone" , 20 , 3);
+    }
+
+    @Override
+    public double calculateConsumption(double distance) {
+        return distance * 5.0;
+    }
+
+    @Override
+    public double getSpeed() {
+        if (getBattery() < 20) {
+            return 16.0;
+        }
+        return super.getSpeed();
     }
 }
