cbb04805cb9e37a270f2e005c67dc4fe5378d684
zuc1fer <zucifer0@gmail.com>
2025-12-19 14:41:58 +0100

    feat: introduce ControlCenter class for managing drone fleet and orders

---
+++ ControlCenter.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class ControlCenter {
+    
+    private List<Drone> fleet;
+    private List<Order> pendingOrders;
+    private Position base;
+    private Map map;
+    private List<Order> processedOrders;
+ 
+}
