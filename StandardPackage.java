cfa1ed25343be8e0f68f1cdf51327e775b5ee06f
zuc1fer <zucifer0@gmail.com>
2025-12-19 14:30:32 +0100

    refactor: rename `Destination` field to `destination` in `StandardPackage` for consistent naming.

--- StandardPackage.java
+++ StandardPackage.java
@@ -1,19 +1,21 @@
 public class StandardPackage implements Deliverable{
 
     private double weight;
-    private Position Destination;
+    private Position destination;
 
     public StandardPackage(double nweight , Position nDestination){
 
         this.weight = nweight;
-        this.Destination = nDestination;
+        this.destination = nDestination;
     }
+
     @Override 
     public double getWeight(){return weight;}
+
     @Override
     public Position getDestination(){return destination;}
 
     public void setWeight(double nweight){this.weight = nweight;}
-    public void setDestination(Position nDestination){this.Destination = nDestination;}
+    public void setDestination(Position nDestination){this.destination = nDestination;}
 
 }
