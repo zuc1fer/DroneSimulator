b2428399e9772cecd43f5b8be20fc7d159114b57
zuc1fer <zucifer0@gmail.com>
2025-12-19 14:59:53 +0100

    feat: add `calculateDeliveryCost` method to `ControlCenter` to compute delivery expenses based on order and drone.

--- ControlCenter.java
+++ ControlCenter.java
@@ -83,4 +83,23 @@ public class ControlCenter {
             return false;
         }
     }
+
+    public double calculateDeliveryCost(Order order, Drone drone) {
+        Position dest = order.getDeliverable().getDestination();
+        
+        double distance = drone.getPosition().distanceTo(dest) * 2;
+        
+        double consumption = drone.calculateConsumption(distance);
+        
+        double operationCost = (distance * 0.1) + (consumption * 0.02) + 20;
+        
+        double initialPrice = order.getCost();
+        double insurance = Math.max(initialPrice * 0.02, 10);
+        
+        if (order.getUrgency().equals("EXPRESS")) {
+            insurance += 20;
+        }
+        
+        return operationCost + insurance;
+    }
 }
